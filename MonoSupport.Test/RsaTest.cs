7a78b0d9fed07618517cccdfe7500a886bfa28a5
xafero <georg.berendt@uni-oldenburg.de>
2017-07-10 23:16:34 +0200

    Initial commit for RSA

---
+++ MonoSupport.Test/RsaTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using MonoSupport.ServiceModel;
+using System.Security.Cryptography;
+
+namespace MonoSupport.Test
+{
+    [TestFixture]
+    public class RsaTest
+    {
+        [Test]
+        public void ShouldGetRsaIdentity()
+        {
+            var provider = new RSACryptoServiceProvider();
+            var pubKey = provider.ToXmlString(false);
+            Assert.AreEqual(243, pubKey.Length);
+            var rsa = new RsaEndpointIdentity(pubKey);
+            Assert.IsNotNull(rsa.IdentityClaim);
+            Assert.AreEqual("identity(http://schemas.xmlsoap.org/ws/2005/05/identity/right/possessproperty:" +
+                            " http://schemas.xmlsoap.org/ws/2005/05/identity/claims/rsa)", rsa.ToString());
+        }
+    }
+}
---
+++ MonoSupport/RsaEndpointIdentity.cs
@@ -0,0 +1,17 @@
+﻿using System.IdentityModel.Claims;
+using System.Security.Cryptography;
+
+namespace MonoSupport.ServiceModel
+{
+    public class RsaEndpointIdentity : System.ServiceModel.RsaEndpointIdentity
+    {
+        RSA rsa;
+
+        public RsaEndpointIdentity(string publicKey) : base((Claim)null)
+        {
+            rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+            Initialize(Claim.CreateRsaClaim(rsa));
+        }
+    }
+}
